e43e4e3fb9fc9f7d99f53554399749ed2ce5e566
Samuel Gerstein <sgerstein2019@fau.edu>
2022-03-29 13:19:53 -0400

    s&m alg

--- ElGamal_functions.cpp
+++ ElGamal_functions.cpp
@@ -1,13 +1,5 @@
 #include "ElGamal.h"
 
-
-//Constructor
-client::client(int message, int p, int alpha) {
-    plaintext = message;
-    prime = p;
-    generator = alpha;
-}
-
 bool primality(int n) {
     if (n == 2 || n == 3)
         return true;
@@ -23,6 +15,41 @@ bool primality(int n) {
 
     return true;
 }
+
+string decimalToBinary(int n) {
+    string result = "";
+    while (n > 0) {
+        result = to_string(n % 2) + result;
+        n /= 2;
+    }
+    cout << result << endl;
+    return result;
+}
+
+int repeated_square_and_multiply(int a, int k, int m)
+{
+    int b = 1;
+    string k_binary = decimalToBinary(k);
+    if (k_binary[(k_binary.length()-1)] == '1')
+    {
+        b = a;
+    }
+    for (int i = 1; i < k_binary.length(); i++) {
+        a = (a * a) % m;
+        if (k_binary[(k_binary.length()-i-1)] == '1') {
+            b = (b * a) % m;
+        }
+    }
+    return b;
+}
+
+//Constructor
+client::client(int message, int p, int alpha) {
+    plaintext = message;
+    prime = p;
+    generator = alpha;
+}
+
 
 void client::generateKeys() {
 
