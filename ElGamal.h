5269803157846e4dd6969151c8129baf3d8b0853
Samuel Gerstein <sgerstein2019@fau.edu>
2022-03-29 12:08:20 -0400

    key generation + primality test

--- ElGamal.h
+++ ElGamal.h
@@ -1,14 +1,15 @@
 #ifndef ELGAMAL_H
 #define ELGAMAL_H
 #include <iostream>
+#include <cmath>
 class client {
     public:
         client(int message, int p, int alpha);
         void generateKeys();
         void encrypt();
         void decrypt();
-        void send_cipher();
-        void get_public_key();
+        int send_cipher();
+        int get_public_key();
     private:
         int plaintext;  // plaintext to be encrypted or found via encryption
         int ciphertext; // ciphertext to be sent or found via decryption
--- ElGamal_functions.cpp
+++ ElGamal_functions.cpp
@@ -8,4 +8,46 @@ client::client(int message, int p, int alpha) {
     generator = alpha;
 }
 
+bool primality(int n) {
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void client::generateKeys() {
+
+    //random large prime number 
+    srand(time(0));
+    unsigned long long int random = pow(10,30) * rand();
+    while (primality(random) != true) {
+        random = pow(10, 30) * rand();
+    }
+    
+}
+
+void client::encrypt() {
+
+}
+
+void client::decrypt() {
+
+}
+
+int client::send_cipher() {
+
+}
+
+int client::get_public_key() {
+
+}
 
